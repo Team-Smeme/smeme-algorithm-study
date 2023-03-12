e984a28d27c427b5be1cd7e892f4684011c81fa8
taeheeL <t9h9@naver.com>
2023-03-12 13:40:28 +0900

    [Week2] 15. 좌표 정렬하기

---
+++ foxs-boy/정렬/좌표정렬하기.java
@@ -0,0 +1,41 @@
+package 정렬;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class 좌표정렬하기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        int[][] arr = new int[count][2];
+
+        StringTokenizer st;
+        for (int i = 0; i < count; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if (e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        }
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+
+
+    }
+}
