a2bcadced0f46dfdc30c1b8424a759755bdb0a65
taeheeL <t9h9@naver.com>
2023-05-01 18:04:23 +0900

    [Week9] 3. 덩치

---
+++ foxs-boy/브루트포스/덩치.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 덩치 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        int count;
+
+        for (int i = 0; i < N; i++) {
+            count = 1;
+            for (int j = 0; j < N; j++) {
+                if (i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    count++;
+                }
+            }
+
+            bw.write(String.valueOf(count) + ' ');
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
